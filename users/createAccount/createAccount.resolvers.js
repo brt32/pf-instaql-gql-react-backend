4d4bfe910805405d63f54e954eca83b56e45122e
Bartek <bartlomiej.proczkowski@gmail.com>
2022-01-29 19:46:49 +0100

    seeProfile & editProfile

---
+++ users/createAccount/createAccount.resolvers.js
@@ -0,0 +1,44 @@
+import client from "../../client";
+import bcrypt from "bcrypt";
+
+export default {
+  Mutation: {
+    createAccount: async (
+      _,
+      { firstName, lastName, username, email, password }
+    ) => {
+      try {
+        // check if user is already on DB
+        const existingUser = await client.user.findFirst({
+          where: {
+            OR: [
+              {
+                username,
+              },
+              {
+                email,
+              },
+            ],
+          },
+        });
+        if (existingUser) {
+          throw new Error("This username/password is already taken.");
+        }
+        console.log("Mutations Working...");
+        // hash password
+        const uglyPassword = await bcrypt.hash(password, 10);
+        return client.user.create({
+          data: {
+            username,
+            email,
+            firstName,
+            lastName,
+            password: uglyPassword,
+          },
+        });
+      } catch (error) {
+        return error;
+      }
+    },
+  },
+};
---
+++ users/editProfile/editProfile.resolvers.js
@@ -0,0 +1,43 @@
+import bcrypt from "bcrypt";
+import client from "../../client";
+import { protectedResolver } from "../users.utils";
+
+export default {
+  Mutation: {
+    editProfile: protectedResolver(
+      async (
+        _,
+        { firstName, lastName, username, email, password: newPassword, bio },
+        { loggedInUser }
+      ) => {
+        let uglyPassword = null;
+        if (newPassword) {
+          uglyPassword = await bcrypt.hash(newPassword, 10);
+        }
+        const updatedUser = await client.user.update({
+          where: {
+            id: loggedInUser.id,
+          },
+          data: {
+            firstName,
+            lastName,
+            username,
+            email,
+            bio,
+            ...(uglyPassword && { password: uglyPassword }), // only if user change a password
+          },
+        });
+        if (updatedUser.id) {
+          return {
+            ok: true,
+          };
+        } else {
+          return {
+            ok: false,
+            error: "Could not update profile",
+          };
+        }
+      }
+    ),
+  },
+};
